93f58179e0dfaf6338908a07db839679883e8a2e
Jan Freyberg <jan.freyberg@gmail.com>
2021-08-31 21:11:39 +0100

    version bump

--- src/testver/__init__.py
+++ src/testver/__init__.py
@@ -5,25 +5,23 @@ import datetime
 import astunparse
 from pathlib import Path
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 
-def modver(filepath: Path, varname='__version__', mode='string'):
+def modver(filepath: Path, varname="__version__", mode="string"):
 
-    if mode == 'string':
+    if mode == "string":
         temp_ver = datetime.datetime.now().strftime("%Y.%m.%d.a%H.dev%M")
-    elif mode == 'tuple':
+    elif mode == "tuple":
         temp_ver = tuple(datetime.datetime.now().utctimetuple())
         temp_ver = tuple(
-            [item for item in temp_ver[:-1]] + [f'dev{temp_ver[-1]}']
+            [item for item in temp_ver[:-1]] + [f"dev{temp_ver[-1]}"]
         )
 
     filepath = Path(filepath)
     tree = ast.parse(filepath.read_text())
     for node in tree.body:
-        if isinstance(node, ast.Assign) and (
-            node.targets[0].id == varname
-        ):
+        if isinstance(node, ast.Assign) and (node.targets[0].id == varname):
             node.value = ast.Constant(value=temp_ver)
             # if isinstance(node.value, ast.Constant):
             #     # python >=3.8 doesn't have ast.Str
