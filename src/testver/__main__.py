e6782b984930fb8ade632a927a29839ae3f22fc1
Jan Freyberg <jan.freyberg@gmail.com>
2021-08-31 21:11:11 +0100

    adding options to testver main

--- src/testver/__main__.py
+++ src/testver/__main__.py
@@ -6,14 +6,19 @@ from . import modver
 
 parser = argparse.ArgumentParser(description="Process some integers.")
 parser.add_argument(
-    "file", type=str, help="The file to read in and modify.",
+    "file",
+    type=str,
+    help="The file to read in and modify.",
 )
 parser.add_argument(
-    "--suffix",
-    "-s",
-    metavar="suffix",
+    "--varname", type=str, default="__version__", required=False
+)
+parser.add_argument(
+    "--mode",
     type=str,
-    help="The suffix to append. The default is to find this from git.",
+    default="string",
+    choices=["string", "tuple"],
+    required=False,
 )
 parser.add_argument("--dryrun", action="store_true")
 
@@ -27,8 +32,8 @@ def main():
 
     args = parser.parse_args()
     filepath = pathlib.Path(args.file)
-
-    suffix = args.suffix or sha
+    varname = args.varname
+    mode = args.mode
 
     if filepath.is_dir():
         files_to_try = sorted(
@@ -36,16 +41,19 @@ def main():
             + [file_ for file_ in sorted(filepath.glob("**/__version__.py"))]
         )
         for file_ in files_to_try:
-            if "__version__" in file_.read_text():
-                mod_text, new_ver = modver(file_)
-                if not args.dryrun:
-                    file_.write_text(mod_text)
-                print(f"Changed __version__ in file {file_} to {new_ver}")
+            if varname in file_.read_text():
+                filepath = file_
+                break
+        else:
+            raise FileNotFoundError(
+                f"Could not find a file that contained the variable {varname}"
+            )
+    mod_text, new_ver = modver(filepath, varname=varname, mode=mode)
+    if not args.dryrun:
+        filepath.write_text(mod_text)
+        print(f"Changed {varname} in file {filepath} to {new_ver}")
     else:
-        mod_text, new_ver = modver(filepath)
-        if not args.dryrun:
-            filepath.write_text(mod_text)
-        print(f"Changed __version__ in file {filepath} to {new_ver}")
+        print(f"Would have changed {varname} in file {filepath} to {new_ver}")
 
 
 if __name__ == "__main__":
